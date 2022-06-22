fd672a5cdb502fa032fb77a3e9c4a26f28d7fedf
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-22 17:25:27 +0000

    got my tweeter feed looking alright, managed to get it all in the article.tweet header

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+
+}
+
+$(document).ready(function() {
+  const createTweetElement = function(obj) {
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); 
+}
+})
+const $tweet = createTweetElement(tweetData);
+
