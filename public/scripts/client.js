d429104ef89da8f0eb9cdf8355b8c66a75faa394
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-22 21:37:06 +0000

    used new function createTweetElement to use dummy data to append new tweets follwing the html and css i coded, made new element with JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,11 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
+
+
+const tweetData = { 
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -17,16 +16,49 @@ const tweetData = {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
   "created_at": 1461116232227
+}
+
+const createTweetElement = function(data) {
+  const $tweet = $(`<article class="tweet">
+  <br>
+  <header>
+    <div class="top-half">
+    <div class="user">
+      <span class="avatar" class="fa-solid fa-user"></span>
+      <div class="name">${data.user.name}</div>
+    </div>
+
+    <div class="handle">${data.user.handle}</div>
+   </div>
+
+    <div class="bottom-half">
+
+      <div class="content">
+       <div class="text">${data.content.text}</div>
+       <div class="border"></div>
+     </div>
+
+     <div class="date-icons">
+       <div class ="date">${data.created_at}</div>
+       <div class="icons">
+         <i class="fa-solid fa-arrows-retweet"></i>
+         <i class="fa-solid fa-flag"></i>
+         <i class="fa-solid fa-heart"></i>
+       </div>
+     </div>
+   </div> 
+
+  </header>
+  
+</article>`);
+return $tweet
 
 }
+
 
 $(document).ready(function() {
-  const createTweetElement = function(obj) {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); 
-}
-})
-const $tweet = createTweetElement(tweetData);
+ const $tweet = createTweetElement(tweetData); 
+ console.log($tweet); 
+ $("#tweets-container").append($tweet)
 
+})
