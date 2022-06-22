542324d32d20f14909221977ad3bee9a855d6e6c
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-22 22:52:02 +0000

    added an event listener on my form/submit, now i jave been able to prevent the default behaviour of loading the page as well as acces the inputed text in the textarea

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,40 +6,66 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 
 
-const tweetData = { 
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+] 
 
-const createTweetElement = function(data) {
+
+
+  const renderTweets = function(arr) {    // loops through tweets
+    arr.forEach(tweet => { // calls createTweetElement for each tweet
+    //  createTweetElement(tweet)// takes return value and appends it to the tweets container  
+     const $tweet = createTweetElement(tweet);
+     $('#tweets-container').append($tweet);
+   }); 
+  
+  }    
+
+
+
+const createTweetElement = function(tweet) {
   const $tweet = $(`<article class="tweet">
   <br>
   <header>
     <div class="top-half">
     <div class="user">
       <span class="avatar" class="fa-solid fa-user"></span>
-      <div class="name">${data.user.name}</div>
+      <div class="name">${tweet.user.name}</div>
     </div>
 
-    <div class="handle">${data.user.handle}</div>
+    <div class="handle">${tweet.user.handle}</div>
    </div>
 
     <div class="bottom-half">
 
       <div class="content">
-       <div class="text">${data.content.text}</div>
+       <div class="text">${tweet.content.text}</div>
        <div class="border"></div>
      </div>
 
      <div class="date-icons">
-       <div class ="date">${data.created_at}</div>
+       <div class ="date">${tweet.created_at}</div>
        <div class="icons">
          <i class="fa-solid fa-arrows-retweet"></i>
          <i class="fa-solid fa-flag"></i>
@@ -52,13 +78,13 @@ const createTweetElement = function(data) {
   
 </article>`);
 return $tweet
-
 }
 
-
 $(document).ready(function() {
- const $tweet = createTweetElement(tweetData); 
- console.log($tweet); 
- $("#tweets-container").append($tweet)
+  renderTweets(data);
+  $('form').submit(function( event ) {
+    console.log(event.target.text.value);
+    event.preventDefault();
+  });
+}) 
 
-})
