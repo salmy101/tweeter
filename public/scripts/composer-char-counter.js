b28f4fa97e1779f8d1c12743f5a1fca11d50076e
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-22 01:20:57 +0000

    finished the charcater counter and goes to negative and turns red

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+   //The this keyword is a reference to the text area
+    let tweetCharacters = $(this).val().length
+    let charactersLeft = 140 - tweetCharacters;
+    // console.log($(this).val().length)
+    // console.log($(this).parent());
+    let $counter = $(this).parent().children("#after-border").children(".counter") //transverse this = text area, child = button, child = counter
+    $counter.text(charactersLeft) //The result of the .text() method is a string containing the combined text of all matched elements
+    if (charactersLeft < 0) {
+        $counter.css("color", "red")
+    }
+
+  });
+
+});
+
+//diff methods to access DOM elements (find,select...)
+
+
+
+
