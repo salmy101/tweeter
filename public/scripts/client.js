9637f646c7872c739cccf3999d44257c3e704b08
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-06-23 02:13:02 +0000

    implented the ajax get request!

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,41 +6,48 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-] 
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ] 
+
+ 
+
+const loadTweets = function() { 
+  $.get("/tweets").then(data => { 
+    renderTweets(data) 
+  }) 
+} ; 
 
 
+const renderTweets = function(arr) {    // loops through tweets
+  arr.forEach(tweet => { // calls createTweetElement for each tweet
+  //  createTweetElement(tweet)// takes return value and appends it to the tweets container  
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }); 
 
-  const renderTweets = function(arr) {    // loops through tweets
-    arr.forEach(tweet => { // calls createTweetElement for each tweet
-    //  createTweetElement(tweet)// takes return value and appends it to the tweets container  
-     const $tweet = createTweetElement(tweet);
-     $('#tweets-container').append($tweet);
-   }); 
-  
-  }    
+}    
 
 
 
@@ -50,7 +57,7 @@ const createTweetElement = function(tweet) {
   <header>
     <div class="top-half">
     <div class="user">
-      <span class="avatar" class="fa-solid fa-user"></span>
+      <span class="avatar">${tweet.user.avatars}</span>
       <div class="name">${tweet.user.name}</div>
     </div>
 
@@ -81,7 +88,7 @@ return $tweet
 }
 
 $(document).ready(function() {
-  renderTweets(data);
+  // renderTweets(data);
   $('form').submit(async( event ) => {
     console.log(event.target.text.value);
     event.preventDefault();
@@ -89,7 +96,7 @@ $(document).ready(function() {
       method:"POST", 
       data: $("#tweet-text").serialize()
     })
-  });
+  }); loadTweets(); 
 }) 
 
 
